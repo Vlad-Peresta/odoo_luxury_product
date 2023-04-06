94bd4e188ffe711a0f943332100dc50b7e24874a
Vladyslav Peresta <perestavlad@gmail.com>
2023-04-06 08:29:27 +0300

    [FIX] change field name is_luxury_product on x_is_luxury_product

--- custom_addons/luxury_product/models/product_template.py
+++ custom_addons/luxury_product/models/product_template.py
@@ -4,12 +4,12 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = ["product.template"]
 
-    is_luxury_product = fields.Boolean(
+    x_is_luxury_product = fields.Boolean(
         string="Is luxury product?",
-        compute="_compute_is_luxury_product",
+        compute="_compute_x_is_luxury_product",
     )
 
-    def _compute_is_luxury_product(self):
+    def _compute_x_is_luxury_product(self):
         luxury_product_ids = self.filtered(
             lambda template: template.attribute_line_ids.filtered(
                 lambda attribute_line: attribute_line.attribute_id.name == "Tag"
@@ -18,5 +18,5 @@ class ProductTemplate(models.Model):
                 lambda value: value.name == "Luxury"
             )
         )
-        luxury_product_ids.is_luxury_product = True
-        (self - luxury_product_ids).is_luxury_product = False
+        luxury_product_ids.x_is_luxury_product = True
+        (self - luxury_product_ids).x_is_luxury_product = False
