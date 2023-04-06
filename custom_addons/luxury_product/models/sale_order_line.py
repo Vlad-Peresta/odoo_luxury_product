308fbc27eb33582b16d2f85cb80e556e3de1a067
Vladyslav Peresta <perestavlad@gmail.com>
2023-04-06 08:26:48 +0300

    [FIX] change field name is_luxury_product on x_is_luxury_product

--- custom_addons/luxury_product/models/sale_order_line.py
+++ custom_addons/luxury_product/models/sale_order_line.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class SaleOrderLine(models.Model):
     _inherit = ["sale.order.line"]
 
-    is_luxury_product = fields.Boolean(related="product_template_id.is_luxury_product")
+    x_is_luxury_product = fields.Boolean(related="product_template_id.x_is_luxury_product")
