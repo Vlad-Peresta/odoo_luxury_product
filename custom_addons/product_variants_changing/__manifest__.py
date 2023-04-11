54c5cc4bfecc8213a44be614503c37d38bd868b5
Vladyslav Peresta <perestavlad@gmail.com>
2023-04-11 22:03:32 +0300

    [ADD] Access Rights to the module

--- custom_addons/product_variants_changing/__manifest__.py
+++ custom_addons/product_variants_changing/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': "Product variants changing",
-    'description': """
-        Changing attributes for products that have already been used
+    "name": "Product variants changing",
+    "description": """
+        Changing product attributes that have already been used
     """,
-    'author': "Vladyslav Peresta",
+    "author": "Vladyslav Peresta",
     "license": "LGPL-3",
-    'website': "https://github.com/Vlad-Peresta",
-    'category': 'Sales',
-    'version': '0.1.1',
-    'depends': ['base'],
-    'data': [
-        'views/views.xml',
-        'views/templates.xml',
+    "website": "https://github.com/Vlad-Peresta",
+    "category": "Sales",
+    "version": "0.1.1",
+    "depends": ["base", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/product_variants_changing_wizard_views.xml",
     ],
 }
