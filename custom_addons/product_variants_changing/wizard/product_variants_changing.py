c9edcf4f93c11150a495bd643a2b78a51810a123
Vladyslav Peresta <perestavlad@gmail.com>
2023-04-18 11:15:15 +0300

    [FIX] returning domains from onchange

--- custom_addons/product_variants_changing/wizard/product_variants_changing.py
+++ custom_addons/product_variants_changing/wizard/product_variants_changing.py
@@ -16,52 +16,51 @@ class ProductVariantsChanging(models.TransientModel):
             )
         ]
 
-    @api.onchange("x_attribute_id")
-    def _onchange_get_x_old_attribute_value_id_domain(self):
-        return {
-            "domain": {
-                "x_old_attribute_value_id": [
-                    (
-                        "id",
-                        "in",
-                        self.env["product.template"]
-                        .browse(self.env.context.get("active_id"))
-                        .attribute_line_ids.value_ids.ids,
-                    ),
-                    ("attribute_id.id", "=", self.x_attribute_id.id),
-                ]
-            }
-        }
-
-    @api.onchange("x_attribute_id", "x_old_attribute_value_id")
-    def _onchange_get_x_new_attribute_value_id_domain(self):
-        return {
-            "domain": {
-                "x_new_attribute_value_id": [
-                    ("attribute_id.id", "=", self.x_attribute_id.id),
-                    ("id", "!=", self.x_old_attribute_value_id.id),
-                ]
-            }
-        }
-
+    x_allowed_value_ids = fields.Many2many(
+        "product.attribute.value", compute="_compute_x_allowed_value"
+    )
     x_attribute_id = fields.Many2one(
         "product.attribute",
         string="Product variant",
         required=True,
-        domain=_get_x_attribute_id_domain,
+        domain=_get_x_attribute_id_domain
     )
     x_old_attribute_value_id = fields.Many2one(
         "product.attribute.value",
         string="Old product variant",
         required=True,
-        domain=_onchange_get_x_old_attribute_value_id_domain,
+        domain="[('id', 'in', x_allowed_value_ids)]"
     )
     x_new_attribute_value_id = fields.Many2one(
         "product.attribute.value",
         string="New product variant",
         required=True,
-        domain=_onchange_get_x_new_attribute_value_id_domain,
+        domain="[('attribute_id', '=', x_attribute_id), ('id', '!=', x_old_attribute_value_id),]"
     )
+
+    @api.depends("x_attribute_id")
+    def _compute_x_allowed_value(self):
+        self.x_allowed_value_ids = self.env["product.attribute.value"].search(
+            [
+                (
+                    "id",
+                    "in",
+                    self.env["product.template"]
+                    .browse(self.env.context.get("active_id"))
+                    .attribute_line_ids.value_ids.ids,
+                ),
+                ("attribute_id.id", "=", self.x_attribute_id.id),
+            ]
+        )
+
+    @api.onchange("x_attribute_id")
+    def _onchange_x_old_new_attribute_value_id(self):
+        self.x_old_attribute_value_id = False
+        self.x_new_attribute_value_id = False
+
+    @api.onchange("x_old_attribute_value_id")
+    def _onchange_x_new_attribute_value_id(self):
+        self.x_new_attribute_value_id = False
 
     def change_product_attribute(self):
         with self.env.cr.savepoint():
